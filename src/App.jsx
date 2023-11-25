ffd311babe8399301e86a542804041b5e5a6d36d
Devraj326 <141002491+Devraj326@users.noreply.github.com>
2023-11-25 17:08:48 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+import HOF from './Components/HOF'
+
+function App() {
+  
+
+  return (
+    <>
+      <HOF/>
+    </>
+  )
+}
+
+export default App
